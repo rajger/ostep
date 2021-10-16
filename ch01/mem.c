84a4a53a968787ffa2d362dba79f05e3d14ca371
rajger <rajdeep.erd@gmail.com>
2021-10-16 16:20:25 +0200

    mem.c

--- ch01/mem.c
+++ ch01/mem.c
@@ -8,12 +8,18 @@ int main(int argc, char *argv[])
     int *p = malloc(sizeof(int));                               // a1
     assert(p != NULL);                                          // terminate if p is null
     printf("(%d) address pointed to by p: %p\n", getpid(), p);  // a2
-    *p =0;                                                      // a3
+    *p =0; 
+    int c = 0;                                                  // a3
 
     while(1) {
         Spin(1);
         *p = *p + 1;
-        printf("(%d) p: %d\n", getpid(), *p);                   // a4
+        c += 1;
+        printf("(%d) p: %d\n", getpid(), *p); 
+        if ( c == 3 ){
+            exit (100);
+            // break;
+        }                  // a4
     }
     return 0;
 }
